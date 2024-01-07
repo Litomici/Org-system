60c4b65708e4efe9b50f7bd4c915021d0e2783db
Litomici <136203611+Litomici@users.noreply.github.com>
2024-01-07 09:59:48 +0100

    tracking templates

--- Litomici_memeber_system/settings.py
+++ Litomici_memeber_system/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from django.core.mail.backends.console import EmailBackend
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'Litomici_memeber_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/"templates"],
+        'DIRS': [BASE_DIR/"templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
