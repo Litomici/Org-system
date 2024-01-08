a0de3de6746c08ba2edaf8376d4f5f067b63e0f2
Litomici <136203611+Litomici@users.noreply.github.com>
2024-01-08 15:44:30 +0100

    path definign via os

--- Litomici_memeber_system/settings.py
+++ Litomici_memeber_system/settings.py
@@ -9,13 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 import json
 import dj_database_url
 from pathlib import Path
 from django.core.mail.backends.console import EmailBackend
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -70,7 +73,7 @@ ROOT_URLCONF = 'Litomici_memeber_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/"templates"],
+        'DIRS':[os.path.join(BASE_DIR, 'templates')],# [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
