82f82b004a71556a123bac09da6a2c718f26812b
Charouzd <72192163+Charouzd@users.noreply.github.com>
2024-01-21 23:15:14 +0100

    hotFix

--- BcWork/Litomici_memeber_system/settings.py
+++ BcWork/Litomici_memeber_system/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/' 
 STATIC_ROOT = BASE_DIR / 'staticFiles'
-LOGOUT_REDIRECT_URL = "login"
+LOGOUT_REDIRECT_URL = "/login"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
